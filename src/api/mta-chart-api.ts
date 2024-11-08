e0bc9fbf7235951302bbf175780ee1141ebb6f4f
Josh Leslie <hello@joshmleslie.com>
2024-11-08 16:01:16 -0500

    update chart-api to use hardcoded data

--- src/api/mta-chart-api.ts
+++ src/api/mta-chart-api.ts
@@ -14,19 +14,22 @@ import {
 } from '../util/mta-chart';
 import { standardizeMTA_API_Data } from './mta-api-obj';
 
-const url2020RowCount = `https://data.ny.gov/resource/py8k-a8wg.json?$query=SELECT%0A%20%20%60station%60%2C%0A%20%20%60line_name%60%2C%0A%20%20date_trunc_ym(%60date%60)%20AS%20%60by_month_date%60%2C%0A%20%20avg(%60exits%60)%20AS%20%60avg_exits%60%0AWHERE%0A%20%20%60date%60%0A%20%20%20%20BETWEEN%20%222020-01-01T00%3A00%3A00%22%20%3A%3A%20floating_timestamp%0A%20%20%20%20AND%20%222020-12-31T23%3A45%3A00%22%20%3A%3A%20floating_timestamp%0AGROUP%20BY%20%60station%60%2C%20%60line_name%60%2C%20date_trunc_ym(%60date%60)%0A%7C%3E%0ASELECT%20count(*)%20AS%20%60__explore_count_name__%60&`;
-const url2020Data = (offset: number) =>
-	`https://data.ny.gov/resource/py8k-a8wg.json?$query=SELECT%0A%20%20%60station%60%2C%0A%20%20%60line_name%60%2C%0A%20%20date_trunc_ym(%60date%60)%20AS%20%60by_month_date%60%2C%0A%20%20avg(%60exits%60)%20AS%20%60avg_exits%60%0AWHERE%0A%20%20%60date%60%0A%20%20%20%20BETWEEN%20%222020-01-01T00%3A00%3A00%22%20%3A%3A%20floating_timestamp%0A%20%20%20%20AND%20%222020-12-31T23%3A45%3A00%22%20%3A%3A%20floating_timestamp%0AGROUP%20BY%20%60station%60%2C%20%60line_name%60%2C%20date_trunc_ym(%60date%60)%0AORDER%20BY%0A%20%20%60station%60%20ASC%20NULL%20FIRST%2C%0A%20%20%60line_name%60%20ASC%20NULL%20FIRST%2C%0A%20%20date_trunc_ym(%60date%60)%20ASC%20NULL%20FIRST%0ALIMIT%20${MTA_DATA_API_LIMIT}%0AOFFSET%20${offset}&`;
-const url2021RowCount = `https://data.ny.gov/resource/uu7b-3kff.json?$query=SELECT%0A%20%20%60station%60%2C%0A%20%20%60line_name%60%2C%0A%20%20date_trunc_ym(%60date%60)%20AS%20%60by_month_date%60%2C%0A%20%20avg(%60exits%60)%20AS%20%60avg_exits%60%0AWHERE%0A%20%20%60date%60%0A%20%20%20%20BETWEEN%20%222021-01-01T00%3A00%3A00%22%20%3A%3A%20floating_timestamp%0A%20%20%20%20AND%20%222021-12-31T23%3A45%3A00%22%20%3A%3A%20floating_timestamp%0AGROUP%20BY%20%60station%60%2C%20%60line_name%60%2C%20date_trunc_ym(%60date%60)%0A%7C%3E%0ASELECT%20count(*)%20AS%20%60__explore_count_name__%60&`;
-const url2021Data = (offset: number) =>
-	`https://data.ny.gov/resource/uu7b-3kff.json?$query=SELECT%0A%20%20%60station%60%2C%0A%20%20%60line_name%60%2C%0A%20%20date_trunc_ym(%60date%60)%20AS%20%60by_month_date%60%2C%0A%20%20avg(%60exits%60)%20AS%20%60avg_exits%60%0AWHERE%0A%20%20%60date%60%0A%20%20%20%20BETWEEN%20%222021-01-01T00%3A00%3A00%22%20%3A%3A%20floating_timestamp%0A%20%20%20%20AND%20%222021-12-31T23%3A45%3A00%22%20%3A%3A%20floating_timestamp%0AGROUP%20BY%20%60station%60%2C%20%60line_name%60%2C%20date_trunc_ym(%60date%60)%0AORDER%20BY%0A%20%20%60station%60%20ASC%20NULL%20FIRST%2C%0A%20%20%60line_name%60%20ASC%20NULL%20FIRST%2C%0A%20%20date_trunc_ym(%60date%60)%20ASC%20NULL%20FIRST%0ALIMIT%20${MTA_DATA_API_LIMIT}%0AOFFSET%20${offset}&`;
-const url2022RowCount =
-	'https://data.ny.gov/resource/k7j9-jnct.json?$query=SELECT%0A%20%20%60unit%60%2C%0A%20%20%60line_name%60%2C%0A%20%20date_trunc_ym(%60date%60)%20AS%20%60by_month_date%60%2C%0A%20%20avg(%60exits%60)%20AS%20%60avg_exits%60%0AGROUP%20BY%20%60unit%60%2C%20%60line_name%60%2C%20date_trunc_ym(%60date%60)%0A%7C%3E%0ASELECT%20count(*)%20AS%20%60__explore_count_name__%60&';
-const url2022Data = (offset: number) =>
-	`https://data.ny.gov/resource/k7j9-jnct.json?$query=SELECT%0A%20%20%60unit%60%2C%0A%20%20%60line_name%60%2C%0A%20%20date_trunc_ym(%60date%60)%20AS%20%60by_month_date%60%2C%0A%20%20avg(%60exits%60)%20AS%20%60avg_exits%60%0AGROUP%20BY%20%60unit%60%2C%20%60line_name%60%2C%20date_trunc_ym(%60date%60)%0AORDER%20BY%0A%20%20%60unit%60%20ASC%20NULL%20FIRST%2C%0A%20%20%60line_name%60%20ASC%20NULL%20FIRST%2C%0A%20%20date_trunc_ym(%60date%60)%20ASC%20NULL%20FIRST%0ALIMIT%20${MTA_DATA_API_LIMIT}%0AOFFSET%20${offset}&`;
-const url2023RowCount = `https://data.ny.gov/resource/wujg-7c2s.json?$query=SELECT%0A%20%20%60station_complex%60%2C%0A%20%20avg(%60ridership%60)%20AS%20%60avg_ridership%60%2C%0A%20%20%60borough%60%2C%0A%20%20date_trunc_ym(%60transit_timestamp%60)%20AS%20%60by_month_transit_timestamp%60%0AWHERE%0A%20%20%60transit_timestamp%60%0A%20%20%20%20BETWEEN%20%222023-01-01T00%3A00%3A00%22%20%3A%3A%20floating_timestamp%0A%20%20%20%20AND%20%222023-12-31T23%3A45%3A00%22%20%3A%3A%20floating_timestamp%0AGROUP%20BY%0A%20%20%60station_complex%60%2C%0A%20%20%60borough%60%2C%0A%20%20date_trunc_ym(%60transit_timestamp%60)%0A%7C%3E%0ASELECT%20count(*)%20AS%20%60__explore_count_name__%60&`;
-const url2023Data = (offset: number) =>
-	`https://data.ny.gov/resource/wujg-7c2s.json?$query=SELECT%0A%20%20%60station_complex%60%2C%0A%20%20avg(%60ridership%60)%20AS%20%60avg_ridership%60%2C%0A%20%20%60borough%60%2C%0A%20%20date_trunc_ym(%60transit_timestamp%60)%20AS%20%60by_month_transit_timestamp%60%0AWHERE%0A%20%20%60transit_timestamp%60%0A%20%20%20%20BETWEEN%20%222023-01-01T00%3A00%3A00%22%20%3A%3A%20floating_timestamp%0A%20%20%20%20AND%20%222023-12-31T23%3A45%3A00%22%20%3A%3A%20floating_timestamp%0AGROUP%20BY%0A%20%20%60station_complex%60%2C%0A%20%20%60borough%60%2C%0A%20%20date_trunc_ym(%60transit_timestamp%60)%0AORDER%20BY%0A%20%20%60station_complex%60%20ASC%20NULL%20FIRST%2C%0A%20%20%60borough%60%20ASC%20NULL%20FIRST%2C%0A%20%20date_trunc_ym(%60transit_timestamp%60)%20ASC%20NULL%20FIRST%0ALIMIT%20${MTA_DATA_API_LIMIT}%0AOFFSET%20${offset}&`;
+/**
+ * Original URLs for data retrieval, kept for posterity
+ * const url2020RowCount = `https://data.ny.gov/resource/py8k-a8wg.json?$query=SELECT%0A%20%20%60station%60%2C%0A%20%20%60line_name%60%2C%0A%20%20date_trunc_ym(%60date%60)%20AS%20%60by_month_date%60%2C%0A%20%20avg(%60exits%60)%20AS%20%60avg_exits%60%0AWHERE%0A%20%20%60date%60%0A%20%20%20%20BETWEEN%20%222020-01-01T00%3A00%3A00%22%20%3A%3A%20floating_timestamp%0A%20%20%20%20AND%20%222020-12-31T23%3A45%3A00%22%20%3A%3A%20floating_timestamp%0AGROUP%20BY%20%60station%60%2C%20%60line_name%60%2C%20date_trunc_ym(%60date%60)%0A%7C%3E%0ASELECT%20count(*)%20AS%20%60__explore_count_name__%60&`;
+ * const url2020Data = (offset: number) =>
+ * `https://data.ny.gov/resource/py8k-a8wg.json?$query=SELECT%0A%20%20%60station%60%2C%0A%20%20%60line_name%60%2C%0A%20%20date_trunc_ym(%60date%60)%20AS%20%60by_month_date%60%2C%0A%20%20avg(%60exits%60)%20AS%20%60avg_exits%60%0AWHERE%0A%20%20%60date%60%0A%20%20%20%20BETWEEN%20%222020-01-01T00%3A00%3A00%22%20%3A%3A%20floating_timestamp%0A%20%20%20%20AND%20%222020-12-31T23%3A45%3A00%22%20%3A%3A%20floating_timestamp%0AGROUP%20BY%20%60station%60%2C%20%60line_name%60%2C%20date_trunc_ym(%60date%60)%0AORDER%20BY%0A%20%20%60station%60%20ASC%20NULL%20FIRST%2C%0A%20%20%60line_name%60%20ASC%20NULL%20FIRST%2C%0A%20%20date_trunc_ym(%60date%60)%20ASC%20NULL%20FIRST%0ALIMIT%20${MTA_DATA_API_LIMIT}%0AOFFSET%20${offset}&`;
+ * const url2021RowCount = `https://data.ny.gov/resource/uu7b-3kff.json?$query=SELECT%0A%20%20%60station%60%2C%0A%20%20%60line_name%60%2C%0A%20%20date_trunc_ym(%60date%60)%20AS%20%60by_month_date%60%2C%0A%20%20avg(%60exits%60)%20AS%20%60avg_exits%60%0AWHERE%0A%20%20%60date%60%0A%20%20%20%20BETWEEN%20%222021-01-01T00%3A00%3A00%22%20%3A%3A%20floating_timestamp%0A%20%20%20%20AND%20%222021-12-31T23%3A45%3A00%22%20%3A%3A%20floating_timestamp%0AGROUP%20BY%20%60station%60%2C%20%60line_name%60%2C%20date_trunc_ym(%60date%60)%0A%7C%3E%0ASELECT%20count(*)%20AS%20%60__explore_count_name__%60&`;
+ * const url2021Data = (offset: number) =>
+ * `https://data.ny.gov/resource/uu7b-3kff.json?$query=SELECT%0A%20%20%60station%60%2C%0A%20%20%60line_name%60%2C%0A%20%20date_trunc_ym(%60date%60)%20AS%20%60by_month_date%60%2C%0A%20%20avg(%60exits%60)%20AS%20%60avg_exits%60%0AWHERE%0A%20%20%60date%60%0A%20%20%20%20BETWEEN%20%222021-01-01T00%3A00%3A00%22%20%3A%3A%20floating_timestamp%0A%20%20%20%20AND%20%222021-12-31T23%3A45%3A00%22%20%3A%3A%20floating_timestamp%0AGROUP%20BY%20%60station%60%2C%20%60line_name%60%2C%20date_trunc_ym(%60date%60)%0AORDER%20BY%0A%20%20%60station%60%20ASC%20NULL%20FIRST%2C%0A%20%20%60line_name%60%20ASC%20NULL%20FIRST%2C%0A%20%20date_trunc_ym(%60date%60)%20ASC%20NULL%20FIRST%0ALIMIT%20${MTA_DATA_API_LIMIT}%0AOFFSET%20${offset}&`;
+ * const url2022RowCount =
+ * 'https://data.ny.gov/resource/k7j9-jnct.json?$query=SELECT%0A%20%20%60unit%60%2C%0A%20%20%60line_name%60%2C%0A%20%20date_trunc_ym(%60date%60)%20AS%20%60by_month_date%60%2C%0A%20%20avg(%60exits%60)%20AS%20%60avg_exits%60%0AGROUP%20BY%20%60unit%60%2C%20%60line_name%60%2C%20date_trunc_ym(%60date%60)%0A%7C%3E%0ASELECT%20count(*)%20AS%20%60__explore_count_name__%60&';
+ * const url2022Data = (offset: number) =>
+ * `https://data.ny.gov/resource/k7j9-jnct.json?$query=SELECT%0A%20%20%60unit%60%2C%0A%20%20%60line_name%60%2C%0A%20%20date_trunc_ym(%60date%60)%20AS%20%60by_month_date%60%2C%0A%20%20avg(%60exits%60)%20AS%20%60avg_exits%60%0AGROUP%20BY%20%60unit%60%2C%20%60line_name%60%2C%20date_trunc_ym(%60date%60)%0AORDER%20BY%0A%20%20%60unit%60%20ASC%20NULL%20FIRST%2C%0A%20%20%60line_name%60%20ASC%20NULL%20FIRST%2C%0A%20%20date_trunc_ym(%60date%60)%20ASC%20NULL%20FIRST%0ALIMIT%20${MTA_DATA_API_LIMIT}%0AOFFSET%20${offset}&`;
+ * const url2023RowCount = `https://data.ny.gov/resource/wujg-7c2s.json?$query=SELECT%0A%20%20%60station_complex%60%2C%0A%20%20avg(%60ridership%60)%20AS%20%60avg_ridership%60%2C%0A%20%20%60borough%60%2C%0A%20%20date_trunc_ym(%60transit_timestamp%60)%20AS%20%60by_month_transit_timestamp%60%0AWHERE%0A%20%20%60transit_timestamp%60%0A%20%20%20%20BETWEEN%20%222023-01-01T00%3A00%3A00%22%20%3A%3A%20floating_timestamp%0A%20%20%20%20AND%20%222023-12-31T23%3A45%3A00%22%20%3A%3A%20floating_timestamp%0AGROUP%20BY%0A%20%20%60station_complex%60%2C%0A%20%20%60borough%60%2C%0A%20%20date_trunc_ym(%60transit_timestamp%60)%0A%7C%3E%0ASELECT%20count(*)%20AS%20%60__explore_count_name__%60&`;
+ * const url2023Data = (offset: number) =>
+ * `https://data.ny.gov/resource/wujg-7c2s.json?$query=SELECT%0A%20%20%60station_complex%60%2C%0A%20%20avg(%60ridership%60)%20AS%20%60avg_ridership%60%2C%0A%20%20%60borough%60%2C%0A%20%20date_trunc_ym(%60transit_timestamp%60)%20AS%20%60by_month_transit_timestamp%60%0AWHERE%0A%20%20%60transit_timestamp%60%0A%20%20%20%20BETWEEN%20%222023-01-01T00%3A00%3A00%22%20%3A%3A%20floating_timestamp%0A%20%20%20%20AND%20%222023-12-31T23%3A45%3A00%22%20%3A%3A%20floating_timestamp%0AGROUP%20BY%0A%20%20%60station_complex%60%2C%0A%20%20%60borough%60%2C%0A%20%20date_trunc_ym(%60transit_timestamp%60)%0AORDER%20BY%0A%20%20%60station_complex%60%20ASC%20NULL%20FIRST%2C%0A%20%20%60borough%60%20ASC%20NULL%20FIRST%2C%0A%20%20date_trunc_ym(%60transit_timestamp%60)%20ASC%20NULL%20FIRST%0ALIMIT%20${MTA_DATA_API_LIMIT}%0AOFFSET%20${offset}&`;
+ */
 const url2024RowCount = `https://data.ny.gov/resource/wujg-7c2s.json?$query=SELECT%0A%20%20%60station_complex%60%2C%0A%20%20avg(%60ridership%60)%20AS%20%60avg_ridership%60%2C%0A%20%20%60borough%60%2C%0A%20%20date_trunc_ym(%60transit_timestamp%60)%20AS%20%60by_month_transit_timestamp%60%0AWHERE%0A%20%20%60transit_timestamp%60%0A%20%20%20%20BETWEEN%20%222024-01-01T00%3A00%3A00%22%20%3A%3A%20floating_timestamp%0A%20%20%20%20AND%20%222024-12-31T23%3A45%3A00%22%20%3A%3A%20floating_timestamp%0AGROUP%20BY%0A%20%20%60station_complex%60%2C%0A%20%20%60borough%60%2C%0A%20%20date_trunc_ym(%60transit_timestamp%60)%0A%7C%3E%0ASELECT%20count(*)%20AS%20%60__explore_count_name__%60&`;
 const url2024Data = (offset: number) =>
 	`https://data.ny.gov/resource/wujg-7c2s.json?$query=SELECT%0A%20%20%60station_complex%60%2C%0A%20%20avg(%60ridership%60)%20AS%20%60avg_ridership%60%2C%0A%20%20%60borough%60%2C%0A%20%20date_trunc_ym(%60transit_timestamp%60)%20AS%20%60by_month_transit_timestamp%60%0AWHERE%0A%20%20%60transit_timestamp%60%0A%20%20%20%20BETWEEN%20%222024-01-01T00%3A00%3A00%22%20%3A%3A%20floating_timestamp%0A%20%20%20%20AND%20%222024-12-31T23%3A45%3A00%22%20%3A%3A%20floating_timestamp%0AGROUP%20BY%0A%20%20%60station_complex%60%2C%0A%20%20%60borough%60%2C%0A%20%20date_trunc_ym(%60transit_timestamp%60)%0AORDER%20BY%0A%20%20%60station_complex%60%20ASC%20NULL%20FIRST%2C%0A%20%20%60borough%60%20ASC%20NULL%20FIRST%2C%0A%20%20date_trunc_ym(%60transit_timestamp%60)%20ASC%20NULL%20FIRST%0ALIMIT%20${MTA_DATA_API_LIMIT}%0AOFFSET%20${offset}&`;
@@ -35,32 +38,28 @@ const urlUnitToStation = `https://data.ny.gov/resource/py8k-a8wg.json?$query=SEL
 
 interface AccessData {
 	[startDate: string]: {
-		data: (offset: any) => string;
-		count: string;
+		dataUrl: string | ((offset: number) => string);
+		countUrl?: string;
 		stationMap?: string;
 	};
 }
 const apiDataByYear: AccessData = {
 	'2020-01-01': {
-		data: url2020Data,
-		count: url2020RowCount,
+		dataUrl: `${process.env.PUBLIC_URL}/data/mta-ridership-exits-2020.json`,
 	},
 	'2021-01-01': {
-		data: url2021Data,
-		count: url2021RowCount,
+		dataUrl: `${process.env.PUBLIC_URL}/data/mta-ridership-exits-2021.json`,
 	},
 	'2022-01-01': {
-		data: url2022Data,
-		count: url2022RowCount,
+		dataUrl: `${process.env.PUBLIC_URL}/data/mta-ridership-exits-2022.json`,
 		stationMap: urlUnitToStation,
 	},
 	'2023-01-01': {
-		data: url2023Data,
-		count: url2023RowCount,
+		dataUrl: `${process.env.PUBLIC_URL}/data/mta-ridership-exits-2023.json`,
 	},
 	'2024-01-01': {
-		data: url2024Data,
-		count: url2024RowCount,
+		dataUrl: url2024Data,
+		countUrl: url2024RowCount,
 	},
 };
 
@@ -73,17 +72,20 @@ export const getChartData = async (
 	if (!(accessDateString in apiDataByYear)) {
 		throw new Error(`No data configured for date ${accessDateString}`);
 	}
-	const routeData: RouteData = {};
+	let routeData: RouteData = {};
 	const apiYearDatum = apiDataByYear[accessDateString];
 	try {
-		const totalRows = await fetch(apiYearDatum.count)
-			.then((r) => r.json())
-			.then((r) => r[0].__explore_count_name__ as number);
+		let totalRows = 0;
+		if (apiYearDatum.countUrl) {
+			totalRows = await fetch(apiYearDatum.countUrl)
+				.then((r) => r.json())
+				.then((r) => r[0].__explore_count_name__ as number);
 
-		dispatchLoadingUpdate({
-			eventLabel: 'mta-api-segment-count',
-			value: totalRows / MTA_DATA_API_LIMIT,
-		});
+			dispatchLoadingUpdate({
+				eventLabel: 'mta-api-segment-count',
+				value: totalRows / MTA_DATA_API_LIMIT,
+			});
+		}
 
 		let stationMap: UnitToStation;
 		if (apiYearDatum?.stationMap) {
@@ -99,39 +101,50 @@ export const getChartData = async (
 			);
 		}
 
-		const allOffsetRequests = [];
-		for (let offset = 0; offset < totalRows - 1; offset += MTA_DATA_API_LIMIT) {
-			allOffsetRequests.push(
-				fetch(apiYearDatum.data(offset)).then((r) => {
-					dispatchLoadingUpdate({
-						eventLabel: 'mta-api-segment-loaded',
-						value: offset,
-					});
-					return r.json();
-				})
-			);
+		if (typeof apiYearDatum.dataUrl === 'function') {
+			const allOffsetRequests = [];
+			for (
+				let offset = 0;
+				offset < totalRows - 1;
+				offset += MTA_DATA_API_LIMIT
+			) {
+				allOffsetRequests.push(
+					fetch(apiYearDatum.dataUrl(offset)).then((r) => {
+						dispatchLoadingUpdate({
+							eventLabel: 'mta-api-segment-loaded',
+							value: offset,
+						});
+						return r.json();
+					})
+				);
+			}
+			(await Promise.all(allOffsetRequests))
+				.flatMap((d) => d)
+				.reduce((acc: any, stationMonthDatum: TurnstileData) => {
+					const apiDatum = standardizeMTA_API_Data(
+						stationMonthDatum,
+						stationMap
+					);
+
+					if (!acc[apiDatum.station]) {
+						acc[apiDatum.station] = {};
+					}
+					if (!acc[apiDatum.station][apiDatum.line_name]) {
+						acc[apiDatum.station][apiDatum.line_name] = {};
+					}
+					acc[apiDatum.station][apiDatum.line_name][apiDatum.by_month_date] =
+						strToTwoDecimals(apiDatum.avg_exits);
+					return acc;
+				}, routeData);
+		} else {
+			routeData = await fetch(apiYearDatum.dataUrl).then((r) => r.json());
 		}
-
-		(await Promise.all(allOffsetRequests))
-			.flatMap(d => d)
-			.reduce((acc: any, stationMonthDatum: TurnstileData) => {
-				const apiDatum = standardizeMTA_API_Data(stationMonthDatum, stationMap);
-
-				if (!acc[apiDatum.station]) {
-					acc[apiDatum.station] = {};
-				}
-				if (!acc[apiDatum.station][apiDatum.line_name]) {
-					acc[apiDatum.station][apiDatum.line_name] = {};
-				}
-				acc[apiDatum.station][apiDatum.line_name][apiDatum.by_month_date] =
-					strToTwoDecimals(apiDatum.avg_exits);
-				return acc;
-			}, routeData);
-		// }
 	} catch (e: any) {
 		console.warn(e);
 		throw new Error(e || 'chart api error');
 	}
+
+	console.log(routeData);
 
 	const boroughData = routeDataToBoroughs(routeData);
 	const boroughChartData = boroughDataToChart(
